2a1c18eed36bdb83426d998ebc13fba36cf95779
Diogo <diogojunior.dev@gmail.com>
2024-11-19 22:55:30 -0300

    Adição da fonte de dados em memoria e criação dos endpoints para acessa a fonte de dados

--- server.js
+++ server.js
@@ -1,9 +1,58 @@
 import express from "express";
 
+const posts = [
+  {
+    id: 1,
+    descricao: "Uma foto teste",
+    imagem: "https://placecats.com/millie/300/150"
+  },
+  {
+    id: 2,
+    descricao: "Gato fofinho dormindo",
+    imagem: "https://placecats.com/millie/300/150"
+  },
+  {
+    id: 3,
+    descricao: "Paisagem incrível",
+    imagem: "https://placecats.com/millie/300/150"
+  },
+  {
+    id: 4,
+    descricao: "Comida deliciosa",
+    imagem: "https://placecats.com/millie/300/150"
+  },
+  {
+    id: 5,
+    descricao: "Cachorro brincando",
+    imagem: "https://placecats.com/millie/300/150"
+  },
+  {
+    id: 6,
+    descricao: "Gráfico de dados",
+    imagem: "https://placecats.com/millie/300/150"
+  }
+];
+
 const app = express();
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Boas Vindas à imersão");
+// Fala pro express se comunicar em json
+app.use(express.json());
+
+function buscaPostPorId(id) {
+    return posts.find((post) => { 
+        return post.id === Number(id);
+    });
+}
+
+app.get("/posts", (req, res) => {
+    res.status(200)
+        .json(posts);
+});
+
+app.get("/posts/:id", (req, res) => {
+    const post = buscaPostPorId(req.params.id);
+    res.status(200)
+        .json(post);
 });
 
 app.listen(3000, () => {
